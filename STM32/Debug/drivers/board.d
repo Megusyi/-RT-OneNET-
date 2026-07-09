f7f834baa0b8f7382d3aedd0f0baa6956daffa54
Megusyi <3472870485@qq.com>
2026-07-09 15:14:46 +0800

    Add files via upload

---
+++ STM32/Debug/drivers/board.d
@@ -0,0 +1,178 @@
+drivers/board.o: ../drivers/board.c \
+ C:\RT-ThreadStudio\workspace\project\rtconfig_preinc.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtthread.h \
+ C:\RT-ThreadStudio\workspace\project/rtconfig.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtdebug.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtdef.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtservice.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtm.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy/rtlegacy.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy/ipc/workqueue_legacy.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/workqueue.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\finsh/finsh.h \
+ C:\RT-ThreadStudio\workspace\project\drivers/board.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/stm32f407xx.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/core_cm4.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_version.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_compiler.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_gcc.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/mpu_armv7.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h \
+ C:\RT-ThreadStudio\workspace\project\drivers/stm32f4xx_hal_conf.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_iwdg.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_tim.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_tim_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_uart.h \
+ C:\RT-ThreadStudio\workspace\project\drivers\include/drv_common.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rthw.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/rtdevice.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/ringbuffer.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/completion.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/dataqueue.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/workqueue.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/waitqueue.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/pipe.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/poll.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/ringblk_buf.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/serial.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/pin.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/sensor.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/pin.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/adc.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/rt_drv_pwm.h
+
+C:\RT-ThreadStudio\workspace\project\rtconfig_preinc.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtthread.h:
+
+C:\RT-ThreadStudio\workspace\project/rtconfig.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtdebug.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtdef.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtservice.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtm.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy/rtlegacy.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy/ipc/workqueue_legacy.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/workqueue.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\finsh/finsh.h:
+
+C:\RT-ThreadStudio\workspace\project\drivers/board.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/stm32f407xx.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/core_cm4.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_version.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_compiler.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_gcc.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/mpu_armv7.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h:
+
+C:\RT-ThreadStudio\workspace\project\drivers/stm32f4xx_hal_conf.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_iwdg.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_tim.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_tim_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_uart.h:
+
+C:\RT-ThreadStudio\workspace\project\drivers\include/drv_common.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rthw.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/rtdevice.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/ringbuffer.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/completion.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/dataqueue.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/workqueue.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/waitqueue.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/pipe.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/poll.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/ringblk_buf.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/serial.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/pin.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/sensor.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/pin.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/adc.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/drivers/rt_drv_pwm.h:
