69a2efb0224294ad490d2bde199a608954695508
Megusyi <3472870485@qq.com>
2026-07-12 13:34:05 +0800

    Add files via upload

---
+++ STM32/linkscripts/Debug/libraries/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.d
@@ -0,0 +1,95 @@
+libraries/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.o: \
+ ../libraries/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.c \
+ C:\RT-ThreadStudio\workspace\project\rtconfig_preinc.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h \
+ C:\RT-ThreadStudio\workspace\project\drivers/stm32f4xx_hal_conf.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/stm32f407xx.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/core_cm4.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_version.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_compiler.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_gcc.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/mpu_armv7.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_iwdg.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_tim.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_tim_ex.h \
+ C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_uart.h
+
+C:\RT-ThreadStudio\workspace\project\rtconfig_preinc.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h:
+
+C:\RT-ThreadStudio\workspace\project\drivers/stm32f4xx_hal_conf.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/stm32f407xx.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/core_cm4.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_version.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_compiler.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/cmsis_gcc.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include/mpu_armv7.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_adc_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_iwdg.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_tim.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_tim_ex.h:
+
+C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_uart.h:
