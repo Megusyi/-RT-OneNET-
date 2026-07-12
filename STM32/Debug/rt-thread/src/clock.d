5f6f75592d0f456804b28509e9f5eb61e4a25c03
Megusyi <3472870485@qq.com>
2026-07-12 12:58:40 +0800

    Add files via upload

---
+++ STM32/Debug/rt-thread/src/clock.d
@@ -0,0 +1,37 @@
+rt-thread/src/clock.o: ../rt-thread/src/clock.c \
+ C:\RT-ThreadStudio\workspace\project\rtconfig_preinc.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rthw.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtthread.h \
+ C:\RT-ThreadStudio\workspace\project/rtconfig.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtdebug.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtdef.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtservice.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtm.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy/rtlegacy.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy/ipc/workqueue_legacy.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/workqueue.h \
+ C:\RT-ThreadStudio\workspace\project\rt-thread\components\finsh/finsh.h
+
+C:\RT-ThreadStudio\workspace\project\rtconfig_preinc.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rthw.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtthread.h:
+
+C:\RT-ThreadStudio\workspace\project/rtconfig.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtdebug.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtdef.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtservice.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\include/rtm.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy/rtlegacy.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy/ipc/workqueue_legacy.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include/ipc/workqueue.h:
+
+C:\RT-ThreadStudio\workspace\project\rt-thread\components\finsh/finsh.h:
---
+++ STM32/Debug/rt-thread/src/subdir.mk
@@ -0,0 +1,55 @@
+################################################################################
+# 自动生成的文件。不要编辑！
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../rt-thread/src/clock.c \
+../rt-thread/src/components.c \
+../rt-thread/src/device.c \
+../rt-thread/src/idle.c \
+../rt-thread/src/ipc.c \
+../rt-thread/src/irq.c \
+../rt-thread/src/kservice.c \
+../rt-thread/src/mem.c \
+../rt-thread/src/mempool.c \
+../rt-thread/src/object.c \
+../rt-thread/src/scheduler.c \
+../rt-thread/src/thread.c \
+../rt-thread/src/timer.c 
+
+OBJS += \
+./rt-thread/src/clock.o \
+./rt-thread/src/components.o \
+./rt-thread/src/device.o \
+./rt-thread/src/idle.o \
+./rt-thread/src/ipc.o \
+./rt-thread/src/irq.o \
+./rt-thread/src/kservice.o \
+./rt-thread/src/mem.o \
+./rt-thread/src/mempool.o \
+./rt-thread/src/object.o \
+./rt-thread/src/scheduler.o \
+./rt-thread/src/thread.o \
+./rt-thread/src/timer.o 
+
+C_DEPS += \
+./rt-thread/src/clock.d \
+./rt-thread/src/components.d \
+./rt-thread/src/device.d \
+./rt-thread/src/idle.d \
+./rt-thread/src/ipc.d \
+./rt-thread/src/irq.d \
+./rt-thread/src/kservice.d \
+./rt-thread/src/mem.d \
+./rt-thread/src/mempool.d \
+./rt-thread/src/object.d \
+./rt-thread/src/scheduler.d \
+./rt-thread/src/thread.d \
+./rt-thread/src/timer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+rt-thread/src/%.o: ../rt-thread/src/%.c
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O0 -ffunction-sections -fdata-sections -Wall  -g -gdwarf-2 -DSOC_FAMILY_STM32 -DSOC_SERIES_STM32F4 -DUSE_HAL_DRIVER -DSTM32F407xx -I"C:\RT-ThreadStudio\workspace\project\drivers" -I"C:\RT-ThreadStudio\workspace\project\drivers\include" -I"C:\RT-ThreadStudio\workspace\project\drivers\include\config" -I"C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include" -I"C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include" -I"C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\RTOS\Template" -I"C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc" -I"C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc\Legacy" -I"C:\RT-ThreadStudio\workspace\project" -I"C:\RT-ThreadStudio\workspace\project\applications" -I"C:\RT-ThreadStudio\workspace\project" -I"C:\RT-ThreadStudio\workspace\project\packages\at_device-latest\class\esp8266" -I"C:\RT-ThreadStudio\workspace\project\packages\at_device-latest\inc" -I"C:\RT-ThreadStudio\workspace\project\packages\cJSON-v1.7.17" -I"C:\RT-ThreadStudio\workspace\project\packages\dht11-latest" -I"C:\RT-ThreadStudio\workspace\project\packages\onenet-latest\inc" -I"C:\RT-ThreadStudio\workspace\project\packages\pahomqtt-latest\MQTTClient-RT" -I"C:\RT-ThreadStudio\workspace\project\packages\pahomqtt-latest\MQTTPacket\src" -I"C:\RT-ThreadStudio\workspace\project\project\packages\at_device-latest\class\esp8266" -I"C:\RT-ThreadStudio\workspace\project\project\packages\at_device-latest\inc" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\dfs\filesystems\devfs" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\dfs\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\sensors" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\finsh" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy\dfs" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\libc\compilers\common\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\libc\compilers\newlib" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\libc\posix\io\poll" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\libc\posix\io\stdio" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\libc\posix\ipc" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\at\at_socket" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\at\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\netdev\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\sal\impl" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\sal\include\dfs_net" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\sal\include\socket\sys_socket" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\sal\include\socket" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\sal\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\libcpu\arm\common" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\libcpu\arm\cortex-m4" -include"C:\RT-ThreadStudio\workspace\project\rtconfig_preinc.h" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+
