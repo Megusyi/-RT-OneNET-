09499d30d5a2a54911d519d3d8e1468abc65baac
Megusyi <3472870485@qq.com>
2026-07-12 13:28:58 +0800

    Add files via upload

---
+++ STM32/linkscripts/Debug/drivers/subdir.mk
@@ -0,0 +1,100 @@
+################################################################################
+# 自动生成的文件。不要编辑！
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/board.c \
+../drivers/drv_adc.c \
+../drivers/drv_can.c \
+../drivers/drv_clk.c \
+../drivers/drv_common.c \
+../drivers/drv_crypto.c \
+../drivers/drv_dac.c \
+../drivers/drv_eth.c \
+../drivers/drv_flash_f4.c \
+../drivers/drv_gpio.c \
+../drivers/drv_hwtimer.c \
+../drivers/drv_lcd.c \
+../drivers/drv_lcd_mipi.c \
+../drivers/drv_lptim.c \
+../drivers/drv_pm.c \
+../drivers/drv_pulse_encoder.c \
+../drivers/drv_pwm.c \
+../drivers/drv_qspi.c \
+../drivers/drv_rtc.c \
+../drivers/drv_sdio.c \
+../drivers/drv_sdram.c \
+../drivers/drv_soft_i2c.c \
+../drivers/drv_spi.c \
+../drivers/drv_usart.c \
+../drivers/drv_usart_v2.c \
+../drivers/drv_usbd.c \
+../drivers/drv_usbh.c \
+../drivers/drv_wdt.c 
+
+OBJS += \
+./drivers/board.o \
+./drivers/drv_adc.o \
+./drivers/drv_can.o \
+./drivers/drv_clk.o \
+./drivers/drv_common.o \
+./drivers/drv_crypto.o \
+./drivers/drv_dac.o \
+./drivers/drv_eth.o \
+./drivers/drv_flash_f4.o \
+./drivers/drv_gpio.o \
+./drivers/drv_hwtimer.o \
+./drivers/drv_lcd.o \
+./drivers/drv_lcd_mipi.o \
+./drivers/drv_lptim.o \
+./drivers/drv_pm.o \
+./drivers/drv_pulse_encoder.o \
+./drivers/drv_pwm.o \
+./drivers/drv_qspi.o \
+./drivers/drv_rtc.o \
+./drivers/drv_sdio.o \
+./drivers/drv_sdram.o \
+./drivers/drv_soft_i2c.o \
+./drivers/drv_spi.o \
+./drivers/drv_usart.o \
+./drivers/drv_usart_v2.o \
+./drivers/drv_usbd.o \
+./drivers/drv_usbh.o \
+./drivers/drv_wdt.o 
+
+C_DEPS += \
+./drivers/board.d \
+./drivers/drv_adc.d \
+./drivers/drv_can.d \
+./drivers/drv_clk.d \
+./drivers/drv_common.d \
+./drivers/drv_crypto.d \
+./drivers/drv_dac.d \
+./drivers/drv_eth.d \
+./drivers/drv_flash_f4.d \
+./drivers/drv_gpio.d \
+./drivers/drv_hwtimer.d \
+./drivers/drv_lcd.d \
+./drivers/drv_lcd_mipi.d \
+./drivers/drv_lptim.d \
+./drivers/drv_pm.d \
+./drivers/drv_pulse_encoder.d \
+./drivers/drv_pwm.d \
+./drivers/drv_qspi.d \
+./drivers/drv_rtc.d \
+./drivers/drv_sdio.d \
+./drivers/drv_sdram.d \
+./drivers/drv_soft_i2c.d \
+./drivers/drv_spi.d \
+./drivers/drv_usart.d \
+./drivers/drv_usart_v2.d \
+./drivers/drv_usbd.d \
+./drivers/drv_usbh.d \
+./drivers/drv_wdt.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/%.o: ../drivers/%.c
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O0 -ffunction-sections -fdata-sections -Wall  -g -gdwarf-2 -DSOC_FAMILY_STM32 -DSOC_SERIES_STM32F4 -DUSE_HAL_DRIVER -DSTM32F407xx -I"C:\RT-ThreadStudio\workspace\project\drivers" -I"C:\RT-ThreadStudio\workspace\project\drivers\include" -I"C:\RT-ThreadStudio\workspace\project\drivers\include\config" -I"C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Device\ST\STM32F4xx\Include" -I"C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\Include" -I"C:\RT-ThreadStudio\workspace\project\libraries\CMSIS\RTOS\Template" -I"C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc" -I"C:\RT-ThreadStudio\workspace\project\libraries\STM32F4xx_HAL_Driver\Inc\Legacy" -I"C:\RT-ThreadStudio\workspace\project" -I"C:\RT-ThreadStudio\workspace\project\applications" -I"C:\RT-ThreadStudio\workspace\project" -I"C:\RT-ThreadStudio\workspace\project\packages\at_device-latest\class\esp8266" -I"C:\RT-ThreadStudio\workspace\project\packages\at_device-latest\inc" -I"C:\RT-ThreadStudio\workspace\project\packages\cJSON-v1.7.17" -I"C:\RT-ThreadStudio\workspace\project\packages\dht11-latest" -I"C:\RT-ThreadStudio\workspace\project\packages\onenet-latest\inc" -I"C:\RT-ThreadStudio\workspace\project\packages\pahomqtt-latest\MQTTClient-RT" -I"C:\RT-ThreadStudio\workspace\project\packages\pahomqtt-latest\MQTTPacket\src" -I"C:\RT-ThreadStudio\workspace\project\project\packages\at_device-latest\class\esp8266" -I"C:\RT-ThreadStudio\workspace\project\project\packages\at_device-latest\inc" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\dfs\filesystems\devfs" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\dfs\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\drivers\sensors" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\finsh" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy\dfs" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\legacy" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\libc\compilers\common\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\libc\compilers\newlib" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\libc\posix\io\poll" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\libc\posix\io\stdio" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\libc\posix\ipc" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\at\at_socket" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\at\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\netdev\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\sal\impl" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\sal\include\dfs_net" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\sal\include\socket\sys_socket" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\sal\include\socket" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\components\net\sal\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\include" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\libcpu\arm\common" -I"C:\RT-ThreadStudio\workspace\project\rt-thread\libcpu\arm\cortex-m4" -include"C:\RT-ThreadStudio\workspace\project\rtconfig_preinc.h" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+
