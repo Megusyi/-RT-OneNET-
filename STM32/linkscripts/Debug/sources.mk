5725847c984d5b1c723de58a47654f4f7b6a8e34
Megusyi <3472870485@qq.com>
2026-07-12 13:46:21 +0800

    Add files via upload

---
+++ STM32/linkscripts/Debug/sources.mk
@@ -0,0 +1,58 @@
+################################################################################
+# 自动生成的文件。不要编辑！
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+OBJS := 
+SECONDARY_FLASH := 
+SECONDARY_SIZE := 
+ASM_DEPS := 
+S_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+applications \
+drivers \
+libraries/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc \
+libraries/CMSIS/Device/ST/STM32F4xx/Source/Templates \
+libraries/STM32F4xx_HAL_Driver/Src/Legacy \
+libraries/STM32F4xx_HAL_Driver/Src \
+packages/at_device-latest/class/esp8266 \
+packages/at_device-latest/samples \
+packages/at_device-latest/src \
+packages/cJSON-v1.7.17 \
+packages/dht11-latest \
+packages/onenet-latest/src \
+packages/pahomqtt-latest/MQTTClient-RT \
+packages/pahomqtt-latest/MQTTPacket/src \
+project/Debug \
+rt-thread/components/dfs/filesystems/devfs \
+rt-thread/components/dfs/src \
+rt-thread/components/drivers/ipc \
+rt-thread/components/drivers/misc \
+rt-thread/components/drivers/sensors \
+rt-thread/components/drivers/serial \
+rt-thread/components/finsh \
+rt-thread/components/legacy/ipc \
+rt-thread/components/libc/compilers/common \
+rt-thread/components/libc/compilers/newlib \
+rt-thread/components/libc/posix/io/poll \
+rt-thread/components/net/at/at_socket \
+rt-thread/components/net/at/src \
+rt-thread/components/net/netdev/src \
+rt-thread/components/net/sal/dfs_net \
+rt-thread/components/net/sal/impl \
+rt-thread/components/net/sal/socket \
+rt-thread/components/net/sal/src \
+rt-thread/libcpu/arm/common \
+rt-thread/libcpu/arm/cortex-m4 \
+rt-thread/src \
+
